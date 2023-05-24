13a5134030210efb0a61d4ad42dc16f1ffedd533
nihachandaluri <niharikachn2003@gmail.com>
2023-05-24 15:47:46 +0530

    changes
    tried some changes but didn't workout

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '0dbf2f9925msh309416e6b692d73p1e1ce4jsnaa2bc28d8b88',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+		'X-RapidAPI-Host': 'national-weather-service.p.rapidapi.com'
 	}
 };
 
