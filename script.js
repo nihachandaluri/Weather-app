8f9ad321075a2b2bc7e931b62b7bd12a2dbfddbc
nihachandaluri <niharikachn2003@gmail.com>
2023-05-24 14:14:11 +0530

    nav bar
    Added nav bar from bootstrap

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0dbf2f9925msh309416e6b692d73p1e1ce4jsnaa2bc28d8b88',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
